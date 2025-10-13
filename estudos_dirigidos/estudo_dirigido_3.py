0b9d1833ac6aaa74ad8809d0062e270b4f6607e1
Ricardo Machado <72943956+ricardoamachado@users.noreply.github.com>
2025-10-13 17:08:30 -0300

    Add ED3 - Detecção de bordas

---
+++ estudos_dirigidos/estudo_dirigido_3.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+def main():
+    
+    img = cv.imread('estudos_dirigidos/figuras/penguins.png', cv.IMREAD_GRAYSCALE)
+    # Definindo o kernel de Sobel na vertical e horizontal.
+    Kv = np.array(
+    [
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1],
+    ]
+)
+    Ku = np.transpose(Kv)
+    # Aplicando os kernels na imagem pela convolução.
+    Iv = cv.filter2D(img, -1, Kv)
+    Iu = cv.filter2D(img, -1, Ku)
+    # Calculando a imagem das bordas.
+    I = np.hypot(Iu, Iv)
+    I = np.uint8(I / I.max() * 255)
+    #Plot da imagem das bordas e das imagens dos gradientes.
+    cv.imshow('Imagem das Bordas', I)
+    cv.imshow('Gradiente Vertical', Iv)
+    cv.imshow('Gradiente Horizontal', Iu)
+    cv.waitKey(0)
+
+if __name__ == "__main__":
+    main()
