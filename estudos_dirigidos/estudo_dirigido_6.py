6ae35db21799fa0be90d43607b91809c6c93d6f4
Ricardo Machado <72943956+ricardoamachado@users.noreply.github.com>
2025-10-15 12:48:11 -0300

    Finalizado ED6 - Kernels

---
+++ estudos_dirigidos/estudo_dirigido_6.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+
+def main():
+    img = cv.imread('estudos_dirigidos/figuras/flowers4.png', cv.IMREAD_GRAYSCALE)
+    # Convertendo a imagem para double e normalizando os valores entre 0 e 1.
+    img_float = img.astype(np.float64) / 255.0
+    # Definindo o kernel média 15x15.
+    kU = np.ones((15, 15)) / 15**2
+    imU = cv.filter2D(img_float, -1, kU)
+    # Definindo o kernel gaussiano sigma = 5 e meia largura = 8.
+    MEIA_LARGURA = 8
+    kernel_size = (2 * MEIA_LARGURA) + 1
+    kG = cv.getGaussianKernel(kernel_size, 5)
+    imG = cv.filter2D(img_float, -1, kG)
+    # Visualização das imagens.
+    cv.imshow('Imagem Float', img_float)
+    cv.imshow('Imagem Media', imU)
+    cv.imshow('Imagem Gaussiana', imG)
+    cv.waitKey(0)
+if __name__ == "__main__":
+    main()
